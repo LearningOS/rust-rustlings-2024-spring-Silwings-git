55d726a7b82242b03282c5fa7cc9837f20355d45
Silwings <1910506332@qq.com>
2024-04-13 17:37:44 +0800

    traits

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -7,14 +7,18 @@
 // Execute `rustlings hint traits1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+use std::ops::Add;
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 impl AppendBar for String {
+    fn append_bar(self) -> Self {
+        self.add("Bar")
+    }
     // TODO: Implement `AppendBar` for type `String`.
+
 }
 
 fn main() {
